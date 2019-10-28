77908c9860c771cd7b5a8739c9641bbe8f7122c3
Juan Petrus <juan.petruss@gmail.com>
2019-10-28 10:02:31 -0400

    ajustes layout ususarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_25_034510) do
+ActiveRecord::Schema.define(version: 2019_10_28_133703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2019_10_25_034510) do
     t.string "chassi"
     t.string "modelo"
     t.integer "ano"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "usuarios", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
